486aa90433cc956c99069abcf54dc4f02d9ac01e
calm1205 <naginani072@gmail.com>
2022-06-05 11:21:40 +0900

    install eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@
  * core nodejs moduleの1つ。
  * デフォルトでnodejsが提供しているmodule
  */
-const path = require("path");
+const path = require('path');
 /**
  * このpluginでcontenthashが書き込まれたhtmlファイルの出力が可能
  * https://github.com/jantimon/html-webpack-plugin#options
  */
-const HtmlWebpackPulgin = require("html-webpack-plugin");
+const HtmlWebpackPulgin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main.tsx",
+  mode: 'development',
+  entry: './src/main.tsx',
   plugins: [
     new HtmlWebpackPulgin({
-      title: "React Sandbox",
-      template: path.resolve(__dirname, "src", "public", "index.html"),
+      title: 'React Sandbox',
+      template: path.resolve(__dirname, 'src', 'public', 'index.html'),
     }),
   ],
   output: {
@@ -24,13 +24,13 @@ module.exports = {
      * bundle後のファイル名が必ず同じファイル名だとコードの差分があってもcacheされたbundleファイルをクライアントが読み込んでしまう。
      * フィンガープリントがあることでコードの差分が出るとファイル名も変わるのでコードの差分がある場合はクライアント側でもcacheを破棄して新しいbundleファイルを読み込める。
      */
-    filename: "bundle.[contenthash].js",
+    filename: 'bundle.[contenthash].js',
     /**
      * __dirnameはnodeの変数で絶対パスを返す。
      * path.resolveを活用するのはパスの区切りが/でない場合もあるため。
      * path.resolveを活用すればパスの区切りを環境によって使い分けて結合してくれる
      */
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
 
     /**
      * pathで指定したコンパイル後のファイルを全削除してからbundleファイルを配置
@@ -40,7 +40,7 @@ module.exports = {
   /**
    * bundleファイルのsource-mapをdevtoolで確認できる。
    */
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   /**
    * https://webpack.js.org/guides/build-performance/#typescript-loader
    */
@@ -48,7 +48,7 @@ module.exports = {
     rules: [
       {
         test: /\.tsx$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
@@ -57,15 +57,15 @@ module.exports = {
    * tsファイルを直接bundleする際にimportの拡張子を補完する。
    */
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
     alias: {
-      "~": path.resolve(__dirname, "src"),
+      '~': path.resolve(__dirname, 'src'),
     },
   },
   devServer: {
     historyApiFallback: true, // 開発中のURL変更を反映
     client: {
-      logging: "error", // ログをerrorしか出さない
+      logging: 'error', // ログをerrorしか出さない
     },
   },
 };
